7d214c28911298fac5e88a9391716cfe31a34473
Casey Flynn <cflynn.us@gmail.com>
2013-11-27 20:51:23 -0500

    adding step to clone front-end and back-end repos

--- developmentSetup.sh
+++ developmentSetup.sh
@@ -33,7 +33,7 @@ h1
 
 cd /tmp
 
-#Install VirtualBox
+# Install VirtualBox
 msg="INSTALLING VirtualBox..."
 h2
 curl http://download.virtualbox.org/virtualbox/4.2.18/VirtualBox-4.2.18-88780-OSX.dmg --output virtuabox.dmg -L
@@ -41,7 +41,7 @@ hdiutil attach ./virtuabox.dmg
 sudo installer -pkg /Volumes/VirtualBox/VirtualBox.pkg -target /Volumes/Macintosh\ HD
 hdiutil detach /Volumes/VirtualBox
 
-#Install Vagrant
+# Install Vagrant
 msg="INSTALLING Vagrant..."
 h2
 curl http://files.vagrantup.com/packages/b12c7e8814171c1295ef82416ffe51e8a168a244/Vagrant-1.3.1.dmg --output vagrant.dmg -L
@@ -49,16 +49,24 @@ hdiutil attach ./vagrant.dmg
 sudo installer -pkg /Volumes/Vagrant/Vagrant.pkg -target /Volumes/Macintosh\ HD
 hdiutil detach /Volumes/Vagrant
 
-#Install Vagrant plugins
+# Install Vagrant plugins
 msg="INSTALLING Vagrant Plugins..."
 h2
 vagrant plugin install vagrant-hostmanager
 
+# Download vm image
 msg="Downloading a vanilla 64bit Ubuntu 12.04 linux distrobution for your development VM Environment..."
 h2
 vagrant box add precise64 http://files.vagrantup.com/precise64.box
 
-#Cloning the git repository into your Desktop...
+# Clone the git repository into your Desktop...
+msg="Cloning cobarClient and cobarAPI into ../"
+h2
+cd ../
+git clone git@github.com:cflynn07/cobarClient.git
+git clone ssh://git@bitbucket.org/cflynn_us/cobarapi.git
+
+# DONE
 msg="Done with setup. To start the application:\nvagrant up"
 h2
 #vagrant up
